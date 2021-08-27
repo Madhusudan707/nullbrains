33d27bf8ceec9d03271ec1ded3bb97f1bea3dadd
XDEV200 <xdev200200@gmail.com>
2021-08-27 20:35:58 +0530

    add: shadow

--- src/Components/Blog/Card.jsx
+++ src/Components/Blog/Card.jsx
@@ -8,7 +8,7 @@ export const Card = ({img,btnText,heading,tagline,altText,bg1,bg2,textColor}) =>
             </div>
             <div className={`${bg2} w-full `}>
                 <div className='relative p-4'>
-                    <button style={{fontSize:"10px"}} className={`absolute  -top-3 left-4 bg-white rounded-lg px-4 py-1 text-sm ${textColor} font-bold`}>{btnText}</button>
+                    <button style={{fontSize:"10px"}} className={`shadow-md absolute  -top-3 left-4 bg-white rounded-lg px-4 py-1 text-sm ${textColor} font-bold`}>{btnText}</button>
                 </div>
                 <div className='p-3 text-white '>
                     <h1 className='text-4xl font-bold mb-4'>{heading}</h1>
